45cb2f236a4bf45370d59e2d80a8a2737c11e775
Kyrillos Maher Fekry Fanous <29901021401214@std.art.asu.edu.eg>
2025-08-10 20:22:37 +0300

    useGetRestaurantById

--- src/core/queryClient.ts
+++ src/core/queryClient.ts
@@ -1,6 +1,7 @@
 // src/queryClient.ts
 import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
-import axios, { type AxiosError } from "axios";
+import type { AxiosError } from "axios";
+import axios from "axios";
 
 interface ValidationError {
 	type: string;

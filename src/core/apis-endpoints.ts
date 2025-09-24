44cc6de89e36c329e872b550710b8deb0f68d062
Kyrillos Maher Fekry Fanous <29901021401214@std.art.asu.edu.eg>
2025-09-24 20:33:40 +0300

    to dev

--- src/core/apis-endpoints.ts
+++ src/core/apis-endpoints.ts
@@ -1,9 +1,9 @@
 // prod
-export const BASE_URL = "https://api.tawila.co.uk"; // Production URL
+// export const BASE_URL = "https://api.tawila.co.uk"; // Production URL
 export const RESTAURANT_ID = "67cadca16c210077db259e7c"; // Restaurant API URL
 
 // dev 
-// export const BASE_URL = "https://api-dev.tawila.co.uk";
+export const BASE_URL = "https://api-dev.tawila.co.uk";
 // export const RESTAURANT_ID = "674b5cdfd4f15cf44da5c090";
 // "https://api.tawila.co.uk";
 
